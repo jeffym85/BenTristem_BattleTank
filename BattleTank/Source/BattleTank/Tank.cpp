7ab7f437516fd6cad1ad03d7511e52b4c80ae64a
Jeff Marshall <jeffym85@gmail.com>
2016-04-17 13:03:30 -0400

    Added starter content and C++ class for Tank Pawn BP

---
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "Tank.h"
+
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+	InputComponent->BindAction("Turret_Clockwise", IE_Pressed, this, &ATank::RotateTurret);
+}
+
+void ATank::RotateTurret()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Rotate_Turret Called"));
+	//Set relative rotation
+	//UChildActorComponent
+}
